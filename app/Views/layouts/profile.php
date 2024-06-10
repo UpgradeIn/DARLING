13f40ebba2c81f891470d938630a6d60a16915e1
aulianafahrian <aulianafahrian@gmail.com>
2024-06-10 17:20:42 +0700

    Update profile layout with navigation and content sections

--- app/Views/layouts/profile.php
+++ app/Views/layouts/profile.php
@@ -1,15 +1,44 @@
-<?= $this->extend('layouts/template') ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<?= $this->section('content') ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $this->renderSection('page_title', true) ?></title>
+
+    <link rel="stylesheet" href="<?= base_url('assets/css/preline.css') ?>">
+    <script src="https://cdn.jsdelivr.net/npm/preline@2.3.0/dist/preline.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <?= $this->extend('layouts/template') ?>
+    <?= $this->section('content') ?>
     <?php $role = session('role') ?>
-    <p><i>tulisan ini dari layouts/profile.php</i></p>
-    <div>
-        <a href="<?= base_url("profile/my-profile")?>">Profil Saya</a>
-    </div>
-    <?php if($role == 'user'): ?>
-        <div>
-            <a href="<?= base_url('profile/my-activity')?>">Aktivitas Saya</a>
+
+    <section class="w-full mx-auto py-5 px-10 mt-20">
+        <div class="grid grid-cols-2">
+            <div class="row-span-2 max-w-xs flex flex-col">
+                <div class="border border-gray-300 dark:border-neutral-700 rounded-md">
+                    <h2 class="text-lg font-semibold px-2 py-2 text-gray-800 dark:text-white">Navigasi Profile</h2>
+                    <ul class="max-w-xs flex flex-col ">
+                        <li class="inline-flex items-center gap-x-2 py-3 px-4 text-m font-medium odd:bg-gray-100 bg-white border border-gray-200 text-gray-800 -mt-px  dark:odd:bg-neutral-800 dark:bg-neutral-900 dark:border-neutral-700 dark:text-white">
+                            <a href="<?= base_url("profile/my-profile") ?>">Profil Saya</a>
+                        </li>
+                        <?php if ($role == 'user') : ?>
+                            <div class="text-m inline-flex items-center gap-x-2 py-3 px-4 text-m font-medium odd:bg-gray-100 bg-white border border-gray-200 text-gray-800 -mt-px  dark:odd:bg-neutral-800 dark:bg-neutral-900 dark:border-neutral-700 dark:text-white">
+                                <a href="<?= base_url('profile/my-activity') ?>">Aktivitas Saya</a>
+                            </div>
+                        <?php endif; ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="cols-span-4">
+                <?= $this->renderSection('content') ?>
+            </div>
         </div>
-    <?php endif; ?>
-    <?= $this->renderSection('content') ?>
-<?= $this->endSection() ?>
+    </section>
+    <?= $this->endSection() ?>
+</body>
+
+</html>
--- app/Views/profile/my-profile.php
+++ app/Views/profile/my-profile.php
@@ -1,17 +1,82 @@
 <?= $this->extend('layouts/profile') ?>
 
 <?= $this->section('page_title') ?>
-    Profile Saya | Damri Course
+Profile Saya | Damri Course
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-    <h1>Ini Profil Saya</h1>
-    <div>
-        <p><?= session('email') ?></p>
-        <p><?= session('nama_lengkap') ?></p>
-        <p><?= session('tanggal_lahir') ?></p>
-        <p><?= session('gender') ?></p>
-        <p><?= session('divisi') ?></p>
-        <p><?= session('jabatan') ?></p>
-    </div>
+<section class="col-span-4 px-5 py-4 sm:px-2 sm:py-2">
+    <div class="">
+        <div class="w-full mx-auto text-3xl font-bold text-center">
+            <h1>Welcome, <?= session('email') ?></h1>
+        </div>
+        <div class="w-full mx-auto text-xl text-center">
+            <h3>Informasi mengenai profile, divisi, dan jabatan</h3>
+        </div>
+        <div >
+            <!-- Grid -->
+             <div class="text-center">
+             <div class="relative inline-block">
+                <img class="rounded-full size-24 mx-auto mt-2" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80" alt="Image Description">
+                <span class="absolute bottom-0 end-0 block size-3.5 rounded-full ring-2 ring-white bg-yellow-400 dark:ring-neutral-900"></span>
+            </div>
+             </div>
+            <div class="space-y-2">
+                <label for="af-submit-app-project-name" class="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-neutral-200">
+                    Nama Lengkap
+                </label>
+
+                <input id="af-submit-app-project-name" type="text" class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Nama Lengkap" value="<?= session('nama_lengkap') ?>">
+            </div>
+
+            <div class="space-y-2">
+                <label for="" class="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-neutral-200">
+                    Email
+                </label>
+
+                <input id="" type="text" class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="email@mail.com" value="<?= session('email') ?>">
+            </div>
+            <div class=" grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-4">
+                <div>
+                    <label for="" class="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-neutral-200">
+                        Tanggal Lahir
+                    </label>
+                    <input type="text" class="w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="dd/mm/yyyy" value="<?= session('tanggal_lahir') ?>">
+                </div>
+                <div>
+                    <label for="" class="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-neutral-200">
+                        Gender
+                    </label>
+                    <input type="text" class="w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Gender" value="<?= session('gender') ?>">
+                </div>
+            </div>
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-4">
+                <div>
+                    <label for="" class="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-neutral-200">
+                        Divisi
+                    </label>
+                    <input type="text" class="w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Divisi" value="<?= session('divisi') ?>">
+                </div>
+                <div>
+                    <label for="" class="inline-block text-sm font-medium text-gray-800 mt-2.5 dark:text-neutral-200">
+                        Jabatan
+                    </label>
+                    <input type="text" class="w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Jabatan" value="<?= session('jabatan') ?>">
+                </div>
+            </div>
+            <!-- button -->
+            <div class="mt-5 flex justify-end gap-x-2">
+                <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800">
+                    Cancel
+                </button>
+                <button type="button" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                    Save changes
+                </button>
+            </div>
+            <!-- end button -->
+        </div>
+
+
+</section>
+
 <?= $this->endSection() ?>
