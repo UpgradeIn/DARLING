9ed96a320ad788f4ba836cc2ddebe32dc15e8cf0
aulianafahrian <aulianafahrian@gmail.com>
2024-06-11 16:42:56 +0700

    Update profile layout and my-activity page

--- app/Views/profile/my-activity.php
+++ app/Views/profile/my-activity.php
@@ -1,9 +1,15 @@
 <?= $this->extend('layouts/profile') ?>
 
 <?= $this->section('page_title') ?>
-    Aktivitas Saya | Damri Course
+Aktivitas Saya | Damri Course
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-    <h1>Ini Aktivitas Saya</h1>
+<div class="w-full mx-auto text-3xl font-bold">
+    <h1>Lanjut Belajar</h1>
+</div>
+<div class="w-full mx-auto text-l">
+    <h3>Yuk lanjut progress belajarmu untuk segera mendapatkan sertifikat</h3>
+</div>
+
 <?= $this->endSection() ?>
