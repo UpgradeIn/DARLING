7d48d4d908b7222714a538131292172bb3af12db
dimasardnt6 <dimasardnt6@gmail.com>
2024-06-11 14:10:55 +0700

    update file

---
+++ app/Views/operator/detail-request.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layouts/template') ?>
+
+<?= $this->section('page_title') ?>
+    Manage Assignment | Damri Course
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+    <h1>Detail Request</h1>
+<?= $this->endSection() ?>
