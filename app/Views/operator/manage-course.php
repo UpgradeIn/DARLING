6812a0fe0bf57edf51f82e7ccd2fc59a679f4767
Fatwaff <fax.mp4@gmail.com>
2024-06-11 16:13:22 +0700

    Fix typo in course type option

--- app/Views/operator/manage-course.php
+++ app/Views/operator/manage-course.php
@@ -259,7 +259,7 @@
                                         <select name="course_type" class="py-3 px-3 pe-9 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600">
                                           <option selected disabled value="">Tipe Course</option>
                                           <option value="mandatory">Wajib</option>
-                                          <option value="opsional">Opsional</option>
+                                          <option value="optional">Opsional</option>
                                         </select>
                                       </div>
                                       <!-- End Course Type -->
