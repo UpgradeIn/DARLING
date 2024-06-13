207375b641f7f5909374e22ac9461396831f41d7
erditona <erditonaushaadam@gmail.com>
2024-06-13 21:03:56 +0700

    Add detail-news.php view file

---
+++ app/Views/operator/detail-news.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layouts/template') ?>
+
+<?= $this->section('page_title') ?>
+Detail News | Damri Course
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+<h1>Detail News</h1>
+<?= $this->endSection() ?>
