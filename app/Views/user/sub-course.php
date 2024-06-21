fc198816d9c7106d3b1e645bcb574a87b60f3fc0
erditona <erditonaushaadam@gmail.com>
2024-06-21 20:09:40 +0700

    Refactor user subCourse view

--- app/Views/user/sub-course.php
+++ app/Views/user/sub-course.php
@@ -37,7 +37,10 @@ Course Page | Damri Course
                       </svg>
                       <span class="text-gray-800 dark:text-neutral-200 group-hover:text-blue-600"><?= $subcourse['sequence'] ?>. <?= $subcourse['title'] ?></span>
                     </a>
-                    <input type="checkbox" class="form-checkbox h-5 w-5 text-gray-600 dark:text-neutral-300">
+                    <svg id="statusBadge" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-check text-slate-300">
+                      <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+                      <path d="m9 12 2 2 4-4" />
+                    </svg>
                   </li>
                 <?php endforeach; ?>
               </ul>
